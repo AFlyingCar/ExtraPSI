5716e4a4831bbf65097808f45702fdb9d74a8536
Tyler Robbins <tyler@familyrobbins.com>
2020-02-06 22:53:19 -0800

    Added empty init() method. Added call to registerAll().

--- src/main/java/com/tgd/xpsi/ExtraPSIMod.java
+++ src/main/java/com/tgd/xpsi/ExtraPSIMod.java
@@ -1,6 +1,7 @@
 package com.tgd.xpsi;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -19,5 +20,11 @@ public class ExtraPSIMod {
     public void preInit(FMLPreInitializationEvent event) {
         // TODO
         // gui handlers???
+
+        ExtraPSISpells.registerAll();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
     }
 }
